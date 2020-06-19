c75e83711fd1848345d35f2ce8be3bad56ee6832
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-19 02:33:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_second_array.push(5)
+  my_second_array << 5
 end
 
 def add_element_to_start_of_array(array, element)
